f333e0d0641e414105987797501f77589660d5ab
Admiin <john20040409@gmail.com>
2020-11-28 14:22:37 +0300

    Теперь окружности случайного цвета!

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import random
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPen, QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from giTT1.ui_file import Ui_MainWindow
 
 
-class CirclesWidget(QMainWindow):
+class CirclesWidget(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
-        self.flag, self.color = False, QColor(255, 204, 0)  # Цвет Яндекса!
+        self.flag = False
         self.list_ellipses = [[]]
         self.setWindowTitle("Git и желтые окружности")
         self.loadUI()
@@ -26,6 +25,7 @@ class CirclesWidget(QMainWindow):
         self.list_ellipses[-1].append(random.randint(0, 480))
         self.list_ellipses[-1].append(random.randint(1, 10))
         self.list_ellipses.append([])
+        self.color = QColor(random.randint(0, 255) for _ in range(3))
         self.repaint()
 
     def paintEvent(self, event):
